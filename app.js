ddb4f1a9b22dfc0d0aac8dd299182247ecec03ea
Elliot <elliotcs30@gmail.com>
2022-09-11 13:55:39 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const session = require('express-session')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+const passport = require('passport')
+
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -7,12 +12,21 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -27,9 +41,11 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
